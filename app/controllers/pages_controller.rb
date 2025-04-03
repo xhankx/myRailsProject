7aa5bc29f260fcae24befe09c4dc6c7bd7caed5a
xhankx <hxu7@academic.rrc.ca>
2025-04-03 18:41:07 -0500

    Adding browse by category function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,4 +13,9 @@ class PagesController < ApplicationController
     @recipe = Recipe.find(params[:id])
     @reviews = @recipe.reviews.includes(:user)
   end
+
+  def category
+    @category = params[:category]
+    @recipes = Recipe.where("dietary_labels LIKE ?", "%#{@category}%")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "home", to: "pages#home", as: :home
   get "recipes", to: "pages#recipes", as: :recipes
   get "recipes/:id", to: "pages#recipe", as: :recipe
+  get "category/:category", to: "pages#category", as: :category
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
