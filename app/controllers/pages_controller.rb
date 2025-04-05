ae4b73a5cf764fb15adc1ded26f267d8d5c46e03
xhankx <hxu7@academic.rrc.ca>
2025-04-05 14:30:14 -0500

    Added comments for the project

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,12 @@
+# ------------------------------------------------------------------
+#
+#    WEBD-3011 (263714) Agile Full Stack Web Development
+#    Name: Hang Xu
+#    Date: 2025-04-05
+#    Description: Rails Intro Project
+#
+# ------------------------------------------------------------------
+
 class PagesController < ApplicationController
   def about
   end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,7 +7,4 @@
 #
 # ------------------------------------------------------------------
 
-class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-end
+class RecipesController < ApplicationController
